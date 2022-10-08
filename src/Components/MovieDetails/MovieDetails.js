b7b9acb4156ed016419a8dbbd0a5285c7a0f5fb1
arminsultana <arminsultana556@gmail.com>
2022-10-08 15:57:38 +0600

    movie detals dynamic data added

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 const MovieDetails = () => {
     const { detailsId } = useParams();
     const [details, setDetails] = useState({})
+   
     
     useEffect(() => {
         const url = ` https://movie-task.vercel.app/api/movie?movieId=${detailsId}`;
@@ -12,36 +13,39 @@ const MovieDetails = () => {
             .then(data => setDetails(data))
 
     }, [])
+    const img= `https://image.tmdb.org/t/p/original/${details?.data?.backdrop_path
+
+}`
 
 
-
-    console.log(details?.data);
+    console.log(details);
     return (
         <section class="text-gray-600 body-font overflow-hidden mid-container">
             <div class="container px-5 py-24 mx-auto">
                 <div class="lg:w-4/5 mx-auto flex flex-wrap">
-                    <img alt='' class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400"/>
+                    <img alt=''class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src={img}/>
                         <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                            <h2 class="text-sm title-font text-gray-500 tracking-widest">MPVIE NAME</h2>
+                            <h2 class="text-sm title-font text-gray-500 tracking-widest">MOVIE NAME</h2>
                             <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{details?.data?.title}</h1>
                             <div class="flex mb-4">
                                 <span class="flex items-center">
-                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-orange-500" viewBox="0 0 24 24">
                                         <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                                     </svg>
-                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-orange-500" viewBox="0 0 24 24">
                                         <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                                     </svg>
-                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-orange-500" viewBox="0 0 24 24">
                                         <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                                     </svg>
-                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-orange-500" viewBox="0 0 24 24">
                                         <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                                     </svg>
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-orange-500" viewBox="0 0 24 24">
                                         <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                                     </svg>
-                                    <span class="text-gray-600 ml-3">4 Reviews</span>
+                                    <span class="text-gray-600 ml-3">{details?.data?.vote_average
+} Votes</span>
                                 </span>
                                 <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
                                     <a class="text-gray-500">
@@ -64,7 +68,7 @@ const MovieDetails = () => {
                             <p class="leading-relaxed">{details?.data?.overview}</p>
                             <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                                 <div class="flex">
-                                    <span class="mr-3">Color</span>
+                                    <span class="mr-3">Status :{details?.data?.status}</span>
                                     <button class="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
                                     <button class="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
                                     <button class="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
